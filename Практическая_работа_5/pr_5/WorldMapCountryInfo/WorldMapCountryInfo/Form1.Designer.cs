a8027166647e4da09b232299a02ecad8447bc1c1
Kulikow Andrey <proi70802@gmail.com>
2026-02-20 10:47:10 +0300

    Add files via upload

---
+++ Практическая_работа_5/pr_5/WorldMapCountryInfo/WorldMapCountryInfo/Form1.Designer.cs
@@ -0,0 +1,164 @@
+﻿namespace WorldMapCountryInfo
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        private void InitializeComponent()
+        {
+            this.gMapControl = new GMap.NET.WindowsForms.GMapControl();
+            this.panelInfo = new System.Windows.Forms.Panel();
+            this.lblPopulationValue = new System.Windows.Forms.Label();
+            this.lblCurrencyValue = new System.Windows.Forms.Label();
+            this.lblCountryName = new System.Windows.Forms.Label();
+            this.pictureBoxFlag = new System.Windows.Forms.PictureBox();
+            this.lblPopulation = new System.Windows.Forms.Label();
+            this.lblCurrency = new System.Windows.Forms.Label();
+            this.panelInfo.SuspendLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFlag)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gMapControl
+            // 
+            this.gMapControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Left)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.gMapControl.Bearing = 0F;
+            this.gMapControl.CanDragMap = true;
+            this.gMapControl.EmptyTileColor = System.Drawing.Color.Navy;
+            this.gMapControl.GrayScaleMode = false;
+            this.gMapControl.HelperLineOption = GMap.NET.WindowsForms.HelperLineOptions.DontShow;
+            this.gMapControl.Location = new System.Drawing.Point(0, 0);
+            this.gMapControl.MarkersEnabled = true;
+            this.gMapControl.MaxZoom = 18;
+            this.gMapControl.MinZoom = 2;
+            this.gMapControl.MouseWheelZoomEnabled = true;
+            this.gMapControl.MouseWheelZoomType = GMap.NET.MouseWheelZoomType.MousePositionAndCenter;
+            this.gMapControl.Name = "gMapControl";
+            this.gMapControl.NegativeMode = false;
+            this.gMapControl.PolygonsEnabled = true;
+            this.gMapControl.RetryLoadTile = 0;
+            this.gMapControl.RoutesEnabled = true;
+            this.gMapControl.ScaleMode = GMap.NET.WindowsForms.ScaleModes.Integer;
+            this.gMapControl.SelectedAreaFillColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(65)))), ((int)(((byte)(105)))), ((int)(((byte)(225)))));
+            this.gMapControl.ShowTileGridLines = false;
+            this.gMapControl.Size = new System.Drawing.Size(600, 450);
+            this.gMapControl.TabIndex = 0;
+            this.gMapControl.Zoom = 2D;
+            this.gMapControl.OnMarkerClick += new GMap.NET.WindowsForms.MarkerClick(this.gMapControl_OnMarkerClick);
+            // 
+            // panelInfo
+            // 
+            this.panelInfo.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom)
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.panelInfo.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.panelInfo.Controls.Add(this.lblPopulationValue);
+            this.panelInfo.Controls.Add(this.lblCurrencyValue);
+            this.panelInfo.Controls.Add(this.lblCountryName);
+            this.panelInfo.Controls.Add(this.pictureBoxFlag);
+            this.panelInfo.Controls.Add(this.lblPopulation);
+            this.panelInfo.Controls.Add(this.lblCurrency);
+            this.panelInfo.Location = new System.Drawing.Point(600, 0);
+            this.panelInfo.Name = "panelInfo";
+            this.panelInfo.Size = new System.Drawing.Size(250, 450);
+            this.panelInfo.TabIndex = 1;
+            // 
+            // lblPopulationValue
+            // 
+            this.lblPopulationValue.AutoSize = true;
+            this.lblPopulationValue.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblPopulationValue.Location = new System.Drawing.Point(15, 380);
+            this.lblPopulationValue.Name = "lblPopulationValue";
+            this.lblPopulationValue.Size = new System.Drawing.Size(0, 15);
+            this.lblPopulationValue.TabIndex = 5;
+            // 
+            // lblCurrencyValue
+            // 
+            this.lblCurrencyValue.AutoSize = true;
+            this.lblCurrencyValue.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.lblCurrencyValue.Location = new System.Drawing.Point(15, 350);
+            this.lblCurrencyValue.Name = "lblCurrencyValue";
+            this.lblCurrencyValue.Size = new System.Drawing.Size(0, 15);
+            this.lblCurrencyValue.TabIndex = 4;
+            // 
+            // lblCountryName
+            // 
+            this.lblCountryName.AutoSize = true;
+            this.lblCountryName.Font = new System.Drawing.Font("Segoe UI", 12F, System.Drawing.FontStyle.Bold);
+            this.lblCountryName.Location = new System.Drawing.Point(15, 220);
+            this.lblCountryName.Name = "lblCountryName";
+            this.lblCountryName.Size = new System.Drawing.Size(0, 21);
+            this.lblCountryName.TabIndex = 3;
+            // 
+            // pictureBoxFlag
+            // 
+            this.pictureBoxFlag.Location = new System.Drawing.Point(15, 15);
+            this.pictureBoxFlag.Name = "pictureBoxFlag";
+            this.pictureBoxFlag.Size = new System.Drawing.Size(220, 150);
+            this.pictureBoxFlag.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pictureBoxFlag.TabIndex = 2;
+            this.pictureBoxFlag.TabStop = false;
+            // 
+            // lblPopulation
+            // 
+            this.lblPopulation.AutoSize = true;
+            this.lblPopulation.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lblPopulation.Location = new System.Drawing.Point(15, 365);
+            this.lblPopulation.Name = "lblPopulation";
+            this.lblPopulation.Size = new System.Drawing.Size(86, 15);
+            this.lblPopulation.TabIndex = 1;
+            this.lblPopulation.Text = "Население:";
+            // 
+            // lblCurrency
+            // 
+            this.lblCurrency.AutoSize = true;
+            this.lblCurrency.Font = new System.Drawing.Font("Segoe UI", 9F, System.Drawing.FontStyle.Bold);
+            this.lblCurrency.Location = new System.Drawing.Point(15, 335);
+            this.lblCurrency.Name = "lblCurrency";
+            this.lblCurrency.Size = new System.Drawing.Size(63, 15);
+            this.lblCurrency.TabIndex = 0;
+            this.lblCurrency.Text = "Валюта:";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(850, 450);
+            this.Controls.Add(this.panelInfo);
+            this.Controls.Add(this.gMapControl);
+            this.Name = "Form1";
+            this.Text = "Интерактивная карта мира";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.panelInfo.ResumeLayout(false);
+            this.panelInfo.PerformLayout();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxFlag)).EndInit();
+            this.ResumeLayout(false);
+        }
+
+        private GMap.NET.WindowsForms.GMapControl gMapControl;
+        private System.Windows.Forms.Panel panelInfo;
+        private System.Windows.Forms.PictureBox pictureBoxFlag;
+        private System.Windows.Forms.Label lblPopulation;
+        private System.Windows.Forms.Label lblCurrency;
+        private System.Windows.Forms.Label lblCountryName;
+        private System.Windows.Forms.Label lblCurrencyValue;
+        private System.Windows.Forms.Label lblPopulationValue;
+    }
+}
+
---
+++ Практическая_работа_5/pr_5/WorldMapCountryInfo/WorldMapCountryInfo/Form1.cs
@@ -0,0 +1,254 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.IO;
+using System.Net.Http;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using GMap.NET;
+using GMap.NET.MapProviders;
+using GMap.NET.WindowsForms;
+using GMap.NET.WindowsForms.Markers;
+using Newtonsoft.Json.Linq;
+
+
+namespace WorldMapCountryInfo
+{
+    public partial class Form1 : Form
+    {
+        private readonly HttpClient _httpClient;
+        private readonly GMapOverlay _markersOverlay;
+        private readonly Dictionary<string, PointLatLng> _countryCenters;
+        private string _googleApiKey = "";
+
+        private readonly string _apiKeyPath = Path.Combine(
+            Environment.GetFolderPath(Environment.SpecialFolder.ApplicationData),
+            "WorldMapCountryInfo",
+            "google_api_key.txt"
+        );
+
+        public Form1()
+        {
+            InitializeComponent();
+            _httpClient = new HttpClient();
+            _markersOverlay = new GMapOverlay("countries");
+            _countryCenters = new Dictionary<string, PointLatLng>();
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            LoadApiKey();
+
+            if (string.IsNullOrWhiteSpace(_googleApiKey))
+            {
+                PromptForApiKey();
+            }
+
+            if (!string.IsNullOrWhiteSpace(_googleApiKey))
+            {
+                // Настройка Google Maps
+                GMapProviders.GoogleMap.ApiKey = _googleApiKey;
+                gMapControl.MapProvider = GMapProviders.GoogleMap;
+            }
+            else
+            {
+                MessageBox.Show(
+                    "API-ключ Google Maps не указан.\n" +
+                    "Перезапустите приложение и введите ключ при запуске.",
+                    "Ошибка",
+                    MessageBoxButtons.OK,
+                    MessageBoxIcon.Warning
+                );
+                Application.Exit();
+                return;
+            }
+
+            gMapControl.Position = new PointLatLng(20, 0);
+            gMapControl.MinZoom = 2;
+            gMapControl.MaxZoom = 18;
+            gMapControl.Zoom = 2;
+
+            gMapControl.Overlays.Add(_markersOverlay);
+
+            _countryCenters["RU"] = new PointLatLng(61.5240, 105.3188);
+            _countryCenters["US"] = new PointLatLng(37.0902, -95.7129);
+            _countryCenters["CN"] = new PointLatLng(35.8617, 104.1954);
+            _countryCenters["DE"] = new PointLatLng(51.1657, 10.4515);
+            _countryCenters["FR"] = new PointLatLng(46.2276, 2.2137);
+            _countryCenters["JP"] = new PointLatLng(36.2048, 138.2529);
+            _countryCenters["BR"] = new PointLatLng(-14.2350, -51.9253);
+            _countryCenters["IN"] = new PointLatLng(20.5937, 78.9629);
+
+            foreach (var kvp in _countryCenters)
+            {
+                var marker = new GMarkerGoogle(kvp.Value, GMarkerGoogleType.blue_dot);
+                marker.ToolTipText = GetCountryNameByCode(kvp.Key);
+                marker.Tag = kvp.Key;
+                _markersOverlay.Markers.Add(marker);
+            }
+
+            gMapControl.OnMarkerClick += gMapControl_OnMarkerClick;
+        }
+
+        private void LoadApiKey()
+        {
+            try
+            {
+                if (File.Exists(_apiKeyPath))
+                {
+                    _googleApiKey = File.ReadAllText(_apiKeyPath).Trim();
+                }
+            }
+            catch { /* игнорируем ошибки */ }
+        }
+
+        private void PromptForApiKey()
+        {
+            using (var form = new Form())
+            {
+                form.Width = 500;
+                form.Height = 200;
+                form.Text = "Введите Google Maps API-ключ";
+
+                var label = new Label
+                {
+                    Left = 10,
+                    Top = 10,
+                    Width = 480,
+                    Text = "Введите свой Google Maps API-ключ:\n" +
+                           "(получить можно в Google Cloud Console → APIs & Services → Credentials)"
+                };
+
+                var textBox = new TextBox
+                {
+                    Left = 10,
+                    Top = 60,
+                    Width = 460,
+                    PasswordChar = '*'
+                };
+
+                var buttonOk = new Button
+                {
+                    Text = "OK",
+                    Left = 380,
+                    Top = 100,
+                    Width = 80
+                };
+                buttonOk.Click += (s, e) =>
+                {
+                    _googleApiKey = textBox.Text.Trim();
+                    if (!string.IsNullOrEmpty(_googleApiKey))
+                    {
+                        try
+                        {
+                            Directory.CreateDirectory(Path.GetDirectoryName(_apiKeyPath));
+                            File.WriteAllText(_apiKeyPath, _googleApiKey);
+                        }
+                        catch { /* игнор */ }
+                    }
+                    form.Close();
+                };
+
+                form.Controls.Add(label);
+                form.Controls.Add(textBox);
+                form.Controls.Add(buttonOk);
+                form.ShowDialog();
+            }
+        }
+
+        private string GetCountryNameByCode(string code)
+        {
+            var names = new Dictionary<string, string>
+            {
+                ["RU"] = "Россия",
+                ["US"] = "США",
+                ["CN"] = "Китай",
+                ["DE"] = "Германия",
+                ["FR"] = "Франция",
+                ["JP"] = "Япония",
+                ["BR"] = "Бразилия",
+                ["IN"] = "Индия"
+            };
+            return names.TryGetValue(code, out var name) ? name : code;
+        }
+
+        private async void gMapControl_OnMarkerClick(GMapMarker item, MouseEventArgs e)
+        {
+            if (e.Button != MouseButtons.Left) return;
+
+            var countryCode = item.Tag?.ToString();
+            if (string.IsNullOrEmpty(countryCode)) return;
+
+            try
+            {
+                await LoadCountryInfoAsync(countryCode);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show($"Ошибка загрузки данных: {ex.Message}", "Ошибка", MessageBoxButtons.OK, MessageBoxIcon.Error);
+            }
+        }
+
+        private async Task LoadCountryInfoAsync(string countryCode)
+        {
+            var url = $"https://restcountries.com/v3.1/alpha/{countryCode}";
+            var response = await _httpClient.GetStringAsync(url);
+            var countries = JArray.Parse(response);
+            var country = countries[0];
+
+            var name = country["translations"]?["rus"]?["official"]?.ToString() ??
+                        country["name"]?["common"]?.ToString() ?? "Неизвестно";
+
+            var flagUrl = country["flags"]?["png"]?.ToString();
+            var population = country["population"]?.ToObject<long>() ?? 0;
+
+            string currencyStr = "—";
+            var currencies = country["currencies"];
+
+            if (currencies != null && currencies.Type == JTokenType.Object)
+            {
+                foreach (var token in currencies)
+                {
+                    if (token is JProperty prop)
+                    {
+                        var curName = prop.Value["name"]?.ToString() ?? "—";
+                        var symbol = prop.Value["symbol"]?.ToString() ?? "";
+                        currencyStr = $"{curName} ({symbol})";
+                        break;
+                    }
+                }
+            }
+
+            lblCountryName.Text = name;
+            lblCurrencyValue.Text = currencyStr;
+            lblPopulationValue.Text = FormatNumber(population);
+
+            if (!string.IsNullOrEmpty(flagUrl))
+            {
+                Stream stream = null;
+                try
+                {
+                    stream = await _httpClient.GetStreamAsync(flagUrl);
+                    var image = Image.FromStream(stream);
+                    pictureBoxFlag.Image = image;
+                }
+                catch
+                {
+                    pictureBoxFlag.Image = null;
+                }
+                finally
+                {
+                    stream?.Dispose();
+                }
+            }
+            else
+            {
+                pictureBoxFlag.Image = null;
+            }
+        }
+
+        private string FormatNumber(long number)
+        {
+            return number.ToString("N0", System.Globalization.CultureInfo.CreateSpecificCulture("ru-RU"));
+        }
+    }
+}
